0ddd55eecef8e78e28eaf5c5b8fd5b494a03226d
Ana Sofia Ricardo Aguilar <anasofiaricardoaguilar@gmail.com>
2023-05-09 17:29:25 -0500

    terminando home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,10 @@
+import { Main } from '../Layouts/Home/Main/Main'
 import { NavBar } from '../Layouts/Home/NavBar/NavBar'
 import './Home.css'
 
 export function Homes() {
     return <div className="home">
         <div className='nav-bar'><NavBar/></div>
+        <div className='main'><Main/></div>
     </div>
 }
